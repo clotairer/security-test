231c23f1171ca9eb400cba73f5fed8923eeb01f8
Clotaire <52157233+clotairer@users.noreply.github.com>
2024-03-09 18:15:12 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,22 +1,10 @@
 // test de XSS
-function toggleDisabledZone(event) 
+
+function toggleAccordion()
 {
-    var clickedZone = event.currentTarget;
-    var zoneToToggle = $(clickedZone).attr('data-toggle');
-    var inputId = $(clickedZone).attr('data-input');
-    var inputValue = $('#' + inputId).not(':checked').length > 0;
-    if (inputValue === false) {
-        $('[data-' + zoneToToggle + ']').removeAttr('disabled');
-        if (zoneToToggle === 'month') {
-            $('#widget_input').prop('disabled', true);
-            $('#widget_input').removeClass('widget_disabled');
-            $('#widget_input').addClass('widget_enabled');
-        }
-    } else {
-        $('[data-' + zoneToToggle + ']').prop('disabled', true);
-        if (zoneToToggle === 'month') {
-            $('#widget_input').addClass('widget_disabled');
-            $('#widget_input').removeClass('widget_enabled');
-        }
-    }
+    var button = $(this)[0];
+    var zone = button.getAttribute('data-target');
+    var buttonId = button.getAttribute('id');
+    $(zone.toString()).slideToggle();
+    $('#' + buttonId + ' a').toggleClass('collapsed');
 }
