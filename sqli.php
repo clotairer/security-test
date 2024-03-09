28b7a3e1c387778d0cd2e142e57546274faf58f2
Clotaire <52157233+clotairer@users.noreply.github.com>
2024-03-09 18:19:31 +0100

    Update sqli.php

--- sqli.php
+++ sqli.php
@@ -12,30 +12,3 @@ $queryStr = "SELECT *
 mysql_query($queryStr, $connection);
 
 
-
-function getValue($key, $default_value = false)
-    {
-        if (empty($key) || !is_string($key)) {
-            return false;
-        }
-
-        if (isset($_POST[$key]) || isset($_GET[$key])) {
-            $value = isset($_POST[$key]) ? $_POST[$key] : $_GET[$key];
-        }
-
-        if (!isset($value)) {
-            $value = $default_value;
-        }
-
-        if (is_string($value)) {
-            return urldecode(preg_replace('/((\%5C0+)|(\%00+))/i', '', urlencode($value)));
-        }
-
-        return $value;
-    }
-
-$queryStr = "SELECT * 
-              FROM table
-              WHERE id = " . getValue('id');
-
-mysql_query($queryStr, $connection);
