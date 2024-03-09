0637406e00c65a938dc7552449c0185514b279c1
Clotaire <52157233+clotairer@users.noreply.github.com>
2024-03-09 18:03:34 +0100

    Update sqli.php

--- sqli.php
+++ sqli.php
@@ -1,9 +1,41 @@
 <?php
 
+$connection = mysql_connect('localhost', 'user', 'pass');
+if (!$connection) {
+    die('Not connected : ' . mysql_error());
+}
+
 $queryStr = "SELECT * 
               FROM table
               WHERE id = " . $_GET['id'];
 
-global $connection;
+mysql_query($queryStr, $connection);
+
+
+
+function getValue($key, $default_value = false)
+    {
+        if (empty($key) || !is_string($key)) {
+            return false;
+        }
+
+        if (isset($_POST[$key]) || isset($_GET[$key])) {
+            $value = isset($_POST[$key]) ? $_POST[$key] : $_GET[$key];
+        }
+
+        if (!isset($value)) {
+            $value = $default_value;
+        }
+
+        if (is_string($value)) {
+            return urldecode(preg_replace('/((\%5C0+)|(\%00+))/i', '', urlencode($value)));
+        }
+
+        return $value;
+    }
+
+$queryStr = "SELECT * 
+              FROM table
+              WHERE id = " . getValue('id');
 
 mysql_query($queryStr, $connection);
