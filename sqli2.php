5ec2f9100b622981313e2b6ab9fc87e8ebd87008
Clotaire <52157233+clotairer@users.noreply.github.com>
2024-03-09 18:30:42 +0100

    Update sqli2.php

--- sqli2.php
+++ sqli2.php
@@ -1,17 +1,5 @@
 <?php
-class ToolsCore
-{
-    /**
-     * Get a value from $_POST / $_GET
-     * if unavailable, take a default value.
-     *
-     * @param string $key Value key
-     * @param mixed $default_value (optional)
-     *
-     * @return mixed Value
-     */
-    public static function getValue($key, $default_value = false)
-    {
+function getValue($key, $default_value = false) {
         if (empty($key) || !is_string($key)) {
             return false;
         }
@@ -29,10 +17,10 @@ class ToolsCore
         }
 
         return $value;
-    }
 }
+
 $queryStr = "SELECT * 
               FROM table
-              WHERE id = " . Tools::getValue('id');
+              WHERE id = " . getValue('id');
 
 mysql_query($queryStr, $connection);
